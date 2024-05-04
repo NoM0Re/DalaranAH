7240b1a49782ecb57856130a4bbc9acb3d457370
Darkie <darkchimu@hotmail.com>
2024-05-04 06:04:58 -0400

    fix(localization): Added 'esES' Translations and fixed Zone Name (#2)

--- DalaranAH/locales/esES.lua
+++ DalaranAH/locales/esES.lua
@@ -3,36 +3,36 @@ local L = LibStub("AceLocale-3.0"):NewLocale("DalaranAH", "esES")
 if not L then return end
 
 -- Essential for addon to work correctly
-L["Like Clockwork"] = "Macchine Macchinose"
+L["Like Clockwork"] = "Como un Reloj"
 L["Brassbolt Mechawrench"] = "Pernolatón Mecallave"
 L["Reginald Arcfire"] = "Reginald Arcofuego"
 
 -- Tooltip
-L["Left-click to target"] = true
+L["Left-click to target"] = "Click izquierdo para seleccionar objetivo"
 
-L["Press "] = true
-L[" to interact with Target"] = true
-L[" or "] = true
-L[" to interact with Mouseover"] = true
+L["Press "] = "Presionar "
+L[" to interact with Target"] = " para interactuar con Objetivo"
+L[" or "] = " o "
+L[" to interact with Mouseover"] = " para interactuar sobre el Ratón"
 
-L["Bind 'Interact with Target' to interact with the Target"] = true
-L["Bind 'Interact with Mouseover' to interact with the Mouseover"] = true
+L["Bind 'Interact with Target' to interact with the Target"] = "Asignar 'Interactuar con Objetivo' para interactuar"
+L["Bind 'Interact with Mouseover' to interact with the Mouseover"] = "Asignar 'Interactuar sobre el Ratón' para interactuar"
 
 -- Options Table
-L["Reset"] = true
-L["Reset to Defaults"] = true
-L["Reset all settings to default?"] = true
+L["Reset"] = "Reset"
+L["Reset to Defaults"] = "Restaurar configuración por Defecto"
+L["Reset all settings to default?"] = "¿Restaurar toda la configuración por Defecto?"
 
-L["General"] = true
-L["Button Size"] = true
+L["General"] = "General"
+L["Button Size"] = "Tamaño del Botón"
 
-L["Additional Functions"] = true
-L["Set Mark on the Auctioneer"] = true
-L["On-Click sets Mark on the Auctioneer"] = true
-L["Select the Mark"] = true
-L["Set Auctioneer as Focus"] = true
-L["On-Click sets Auctioneer as Focus"] = true
+L["Additional Functions"] = "Funciones Adicionales"
+L["Set Mark on the Auctioneer"] = "Poner una Marca sobre el Subastador"
+L["On-Click sets Mark on the Auctioneer"] = "Poner una Marca sobre el Subastador al Clickear"
+L["Select the Mark"] = "Seleccionar Marca"
+L["Set Auctioneer as Focus"] = "Establecer Foco en Subastador"
+L["On-Click sets Auctioneer as Focus"] = "Establecer Foco en Subastador al Clickear"
 
-L["Keybinds"] = true
-L["Set Keybind to Interact with Mouseover"] = true
-L["Set Keybind to Interact with Target"] = true
+L["Keybinds"] = "Asignación de Teclas"
+L["Set Keybind to Interact with Mouseover"] = "Asignar tecla para interactuar sobre el Ratón"
+L["Set Keybind to Interact with Target"] = "Asignar tecla para interactuar con el Objetivo"
